25b89c9943bedb24f5ab353d39f9b80a98013fa5
Shmuel Jacobs <shmuel.jacobs@temple.edu>
2017-04-09 01:23:29 -0400

    Ready to submit. All methods work and JUnit test is really a full demo.

--- Assign6_Shmuel_Jacobs/test/compTree.java
+++ Assign6_Shmuel_Jacobs/test/compTree.java
@@ -16,15 +16,20 @@ import static org.junit.Assert.*;
  * Test package for methods required in Assignment 6.
  * @author Shmuel Jacobs
  */
-public class BSTgetsTests {
+public class compTree {
     
     BinarySearchTree<Character> myTree;
+    BinarySearchTree<Integer> checkTree;
+    String testReport;
     
-    public BSTgetsTests() {
+    public compTree() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        System.out.println("Each tree, or pair of trees, printed "
+                + "is followed by a statement\nexplaining some signifigance of"
+                + " tests performed.\n\n");
     }
     
     @AfterClass
@@ -47,41 +52,65 @@ public class BSTgetsTests {
         //empty tree don't throw and return 0
         assertEquals(0, myTree.depthOfMinValueIterative() );
         assertEquals(0, myTree.depthOfMinValueRecursive() );
+        System.out.println(myTree.toString());
+        testReport = "Empty Trees' nodes have no depth.\n";
+        System.out.println(testReport);
         
         //root leaves don't throw and return 1
         myTree.add('h');
         assertEquals(1, myTree.depthOfMinValueIterative() );
         assertEquals(1, myTree.depthOfMinValueRecursive() );
+        System.out.println(myTree.toString());
+        testReport = "Root of tree has depth of 1.\n";
+        System.out.println(testReport);
         
         //add right child and see that it changes nothing
         myTree.add('z');
         assertEquals(1, myTree.depthOfMinValueIterative() );
         assertEquals(1, myTree.depthOfMinValueRecursive() );
+        System.out.println(myTree.toString());
+        testReport = "Even with greater values present, " + testReport;
+        System.out.println(testReport);
         
         //add to both subtrees and check that only left child is counted
         myTree.add('a');
         myTree.add('i');
         assertEquals(2, myTree.depthOfMinValueIterative() );
         assertEquals(2, myTree.depthOfMinValueRecursive() );
+        System.out.println(myTree.toString());
+        testReport = "Depth of minimum value in tree correct: 2\n";
+        System.out.println(testReport);
     }
     
     @Test
     public void structCheckEmptyOneSmallBig(){
         myTree = new BinarySearchTree<Character>();
-        BinarySearchTree<Integer> checkTree = new BinarySearchTree<>();
+        checkTree = new BinarySearchTree<Integer>();
         
         //trees are empty - same structure
         assertTrue(myTree.equalStruct(checkTree));
         assertTrue(checkTree.equalStruct(myTree));
         
+        System.out.println(myTree.toString());
+        System.out.println(myTree.toString());
+        printTrees();
+        testReport = "Empty trees have same structure.\n";
+        System.out.println(testReport);
+        
         //add one, equalsStruct is false
         myTree.add('h');
         assertFalse(myTree.equalStruct(checkTree));
+        printTrees();
+        testReport = "Leaf doesn't have same structure as empty tree.\n";
+        System.out.println(testReport);
         
-        //add one to checktree, to make structures equal again
+        //add one to check tree, to make structures equal again
         checkTree.add(5);
         assertTrue(myTree.equalStruct(checkTree));
         assertTrue(checkTree.equalStruct(myTree));
+        printTrees();
+        testReport = "Leaves have same shape.\n";
+        System.out.println(testReport);
         
         //put three nodes in both, at different places in right subtree
         checkTree.add(3);
@@ -93,6 +122,9 @@ public class BSTgetsTests {
         myTree.add('w');
         
         assertFalse(myTree.equalStruct(checkTree));
+        printTrees();
+        testReport = "Trees have different shapes.\n";
+        System.out.println(testReport);
         
         //fill trees and make them match again
         
@@ -101,5 +133,15 @@ public class BSTgetsTests {
         
         assertTrue(myTree.equalStruct(checkTree));
         assertTrue(checkTree.equalStruct(myTree));
+        
+        printTrees();
+        testReport = "Algorithm can determine that large trees have "
+                + "same structure.\n";
+        System.out.println(testReport);
+    }
+    
+    private void printTrees(){
+        System.out.println(myTree.toString());
+        System.out.println(checkTree.toString());
     }
 }
